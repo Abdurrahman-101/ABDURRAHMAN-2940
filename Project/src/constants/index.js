130f883e7900a8f899c9e76fdcc6e5ff292028b5
Abdurrahman Ihthisham <125637144+Abdurrahman-101@users.noreply.github.com>
2023-11-22 12:57:20 +0530

    Add files via upload

---
+++ Project/src/constants/index.js
@@ -0,0 +1,76 @@
+export const sortCategoryData = ['All', 'Popular', 'Recommended', 'More'];
+
+export const categoriesData = [
+    {
+        title: 'Ocean',
+        image: require('../../assets/images/ocean.png')
+    },
+    {
+        title: 'Mountain',
+        image: require('../../assets/images/mountain.png')
+    },
+    {
+        title: 'Camp',
+        image: require('../../assets/images/camp.png')
+    },
+    {
+        title: 'Sunset',
+        image: require('../../assets/images/sunset.png')
+    },
+    {
+        title: 'Hiking',
+        image: require('../../assets/images/hiking.png')
+    },
+    {
+        title: 'Beach',
+        image: require('../../assets/images/beach.png')
+    },
+    {
+        title: 'Forest',
+        image: require('../../assets/images/forest.png')
+    },
+    
+]
+export const destinationData = [
+    {
+        title: 'Nine Arch Bridge',
+        duration: '3 Days',
+        distance: '400 KM',
+        weather: '20 C',
+        price: 1200,
+        shortDescription: "The Nine Arch Bridge also called the Bridge in the Sky, is a viaduct bridge in Sri Lanka and is one of the best examples of colonial-era railway construction in the nation.",
+        longDescription: "A nine arch bridge is a type of bridge that features nine arches in its design. These arches can be made of various materials such as stone, concrete, or steel, and may be used to span narrow or wide bodies of water, including rivers, canals, and oceans. Some examples of famous nine arch bridges include the Millau Viaduct in France, the Pont Du Gard in France, and the Pantheon in Rome, Italy.",
+        image: require('../../assets/images/ella.jpg')
+    },
+    {
+        title: 'Dambulla Royal Cave Temple',
+        duration: '7 Days',
+        distance: '450 KM',
+        weather: '30 C',
+        price: 3000,
+        shortDescription: "This ancient cave temple complex is the largest and best-preserved temple complex in Sri Lanka.  ",
+        longDescription: "THROUGH THE GOLDEN MOUTH OF a lion-like creature and a short hike up a hill are five gorgeous caves that are home to some of the most magnificent temples in Sri Lanka. The caves contain larger-than-life statues and elaborate, vibrant paintings of Buddha.",
+        image: require('../../assets/images/img.jpg')
+    },
+    
+    {
+        title: 'Lotus Tower',
+        duration: '5 Days',
+        distance: '299 KM',
+        weather: '14 C',
+        price: 1000,
+        shortDescription: "In the heart of Colombo stands a picturesque marvel. The Lotus Tower, also known as the “Nelum Kuluna",
+        longDescription: "In the heart of Colombo stands a picturesque marvel. The Lotus Tower, also known as the “Nelum Kuluna” in Sinhala is the 11th tallest tower in Asia and the 19th tallest tower in the world. The Lotus Tower is a destination like no other, displaying the latest innovations and technologies.",
+        image: require('../../assets/images/lotustower.jpg')
+    },
+    {
+        title: 'Sigiriya',
+        duration: '5 Days',
+        distance: '604 KM',
+        weather: '34 C',
+        price: 400,
+        shortDescription: "Sigiriya is an ancient rock fortress located in central Sri Lanka.",
+        longDescription: " Sigiriya is an ancient rock fortress located in central Sri Lanka. It is considered one of the best-preserved examples of ancient urban planning and showcases advanced architectural and engineering skills. The site is famous for its mirror wall, frescoes, and Gardens of Paradise. Sigiriya is also a UNESCO World Heritage Site and a popular tourist destination.",
+        image: require('../../assets/images/sigiriya.jpg')
+    },
+]
